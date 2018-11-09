887a984af5d383eb93049d43bf8ca139f551ddd9
Alejandro Moreau <moreaualejandro@gmail.com>
2018-11-09 18:22:15 -0400

    upload

--- myapp/database/db/index.js
+++ myapp/database/db/index.js
@@ -1,18 +1,18 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+/*const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
   database: 'inmobiliaria'
-});
+});*/
 
-/*const connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host: 'db4free.net',
   user: 'inmueble',
   password: 'inmueblepass',
   database: 'inmueble'
-});*/
+});
 
 connection.connect((err) => {
   if (err) throw err;
